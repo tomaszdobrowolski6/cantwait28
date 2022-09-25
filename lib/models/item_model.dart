6b174fadfa0641d8ad792b899ec6e94776629c40
Tomasz Dobrowolski <tomasz.dobrowolski@yahoo.com>
2022-09-25 12:37:04 +0200

    Implement Item Model

---
+++ lib/models/item_model.dart
@@ -0,0 +1,13 @@
+class ItemModel {
+  ItemModel({
+    required this.id,
+    required this.tittle,
+    required this.imageURL,
+    required this.relaseDate,
+  });
+
+  final String id;
+  final String tittle;
+  final String imageURL;
+  final DateTime relaseDate;
+}
