d3cca256ee2da6d914032218fabe4fae0c8b402b
Tomasz Dobrowolski <tomasz.dobrowolski@yahoo.com>
2022-09-25 13:48:15 +0200

    Separate logic to items repository

--- lib/models/item_model.dart
+++ lib/models/item_model.dart
@@ -10,4 +10,8 @@ class ItemModel {
   final String tittle;
   final String imageURL;
   final DateTime relaseDate;
+
+  String daysLeft() {
+    return relaseDate.difference(DateTime.now()).inDays.toString();
+  }
 }
